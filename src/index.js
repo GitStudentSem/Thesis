45f694d9deb5d84eaa1a1f0bc5623a06772a98b0
GitStudentSem <programmSem@yandex.ru>
2021-05-13 17:56:27 +0300

    try start webpack

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Не понимаю какие файлы здесь импортировать, а какие нет, при сборке стили не подключаются совсем, в какой-то момент сборка вообще перестала собиратся
+// "use strict";
+// import "./index.html";
+// import "./images";
+// import "./css/animate.css";
+// import "./css/bootstrap.css";
+// import "./css/font-awesome.css";
+// import "./css/jquery.fancybox.css";
+// import "./css/lightgallery.min.css";
+// import "./css/settings.css";
+// import "./css/style.css";
