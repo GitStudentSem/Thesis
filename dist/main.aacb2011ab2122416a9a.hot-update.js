bba9f914af524ae7f74716d702800647a931d87f
GitStudentSem <programmSem@yandex.ru>
2021-05-14 12:46:25 +0300

    create webpack

---
+++ dist/main.aacb2011ab2122416a9a.hot-update.js
@@ -0,0 +1,54 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdateThesis"]("main",{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_testing__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/testing */ \"./src/modules/testing.js\");\n/* harmony import */ var _modules_testing__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_modules_testing__WEBPACK_IMPORTED_MODULE_0__);\n// Здесь хранятся выховы и импорты функций из modules\n\n\n\n_modules_testing__WEBPACK_IMPORTED_MODULE_0___default()();\n\n//# sourceURL=webpack://Thesis/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/modules/testing.js":
+/*!********************************!*\
+  !*** ./src/modules/testing.js ***!
+  \********************************/
+/***/ (() => {
+
+eval("\n\n//# sourceURL=webpack://Thesis/./src/modules/testing.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/compat get default export */
+/******/ (() => {
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = (module) => {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			() => (module['default']) :
+/******/ 			() => (module);
+/******/ 		__webpack_require__.d(getter, { a: getter });
+/******/ 		return getter;
+/******/ 	};
+/******/ })();
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("371f751f8e869430bc70")
+/******/ })();
+/******/ 
+/******/ }
+);
